36699db34d2a3b7f1d35ad16f7515045bbc60df6
diego-molina-cenfotec <dmolinat@ucenfotec.ac.cr>
2022-11-03 00:55:35 -0600

    commit inicial solo para iniciar repositorio en github

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module com.example.directoryclothingui {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires com.almasb.fxgl.all;
+
+    opens com.example.directoryclothingui to javafx.fxml;
+    exports com.example.directoryclothingui;
+}
